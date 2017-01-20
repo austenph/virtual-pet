a60126fb049cb588dfd82a6e38dbc856dd08aa0d
Austen Perelman-Hall <austenph@gmail.com>
2017-01-20 11:31:17 -0500

    this is my first commit. Started creating Virtual Pet Class.

---
+++ VirtualPet/VirtualPet.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualPet
+{
+    class VirtualPet
+    {
+        private string name = "unnamed dragon ";
+        static private string pet_type = "Dragon";
+        private string color = "no color ";
+        private int age = 0;
+    }
+}
