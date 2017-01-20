92508f1237ba0684051c73283b79183ded853ffd
Austen Perelman-Hall <austenph@gmail.com>
2017-01-20 16:28:01 -0500

    working on methods in class

--- VirtualPet/Program.cs
+++ VirtualPet/Program.cs
@@ -10,6 +10,71 @@ namespace VirtualPet
     {
         static void Main(string[] args)
         {
+          
+            //This section sets the storyline up for the user
+            Console.WriteLine("Welcome to Dragon Hatcher!");
+            Console.WriteLine("The interactive pet game were you hatch and raise your very own dragon!");
+            Console.WriteLine("Let's Start!");
+            Console.WriteLine("\nWe are at the Dragon Egg Store.\nPlease select which egg you want from the options below (enter a number 1-5): ");
+            Console.WriteLine("1. Blue Egg for $50");
+            Console.WriteLine("2. Purple Egg for $100");
+            Console.WriteLine("3. Green Egg for $110");
+            Console.WriteLine("4. White Egg for $150");
+            Console.WriteLine("5. Gold Egg for $200");
+
+            Console.Write("\nYour number is: ");  
+            int option = int.Parse(Console.ReadLine());
+        
+            switch(option)
+            {
+                case 1:
+                Console.WriteLine("\nYou selected: Blue Egg for $50!");
+                    break;
+                case 2:
+                    Console.WriteLine("\nYou selected: Purple Egg for $100!");
+                    break;
+                case 3:
+            Console.WriteLine("\nYou selected: Green Egg for $110!");
+                    break;
+                case 4:
+            Console.WriteLine("\nYou selected: White Egg for $150!");
+                    break;
+                case 5:
+            Console.WriteLine("\nYou selected: Gold Egg for $200!");
+                    break;
+                default:
+                    break;
+        }
+            Console.WriteLine("You are now the proud owner of a dragon!");
+            Console.Write("\nA week has past and your egg is ready to Hatch. You must rub the egg three times");
+            Console.Write("to hatch it. Type \"Hatch\" to start hatching your dragon!  ");
+            string hatch = Console.ReadLine();
+            Console.WriteLine(Hatch(hatch));
+            Console.WriteLine("\nNow it is time to start taking care of your new pet Dragon.");
+            Console.WriteLine("Let's start by naming your pet! Enter the name you want for your dragon below: ");
+            VirtualPet pet = new VirtualPet("Dragon");
+            pet.GetName = Console.ReadLine();
+            Console.WriteLine("You have named your dragon " + pet.GetName + "! Great Name!");
+            Console.WriteLine("Now you can interact with " + pet.GetName + "! \nWe will choose from a menu of options in order to raise him:\n" );
+
+           // do
+            
+                pet.MenuDisplayNameandDragon();//always prints out dragons name and the words The Dragon
+            Console.WriteLine("Please select which interation you want from the options below(enter a number 1 - 5): ");
+                Console.WriteLine("1.Play\n2.Train\n3.Hunt\n4.Eat\n5.Fly\n6.Sleep\n7.Bathroom");
+
+
+
+
+            //} while(true);
+
+
+        }
+        //hatch method
+        static string Hatch (string hatch)
+        {
+            hatch= ("\n1.....2......3.... Your Dragon has hatched!!\nCongrats, its a boy!");
+            return hatch;
         }
     }
 }
--- VirtualPet/VirtualPet.cs
+++ VirtualPet/VirtualPet.cs
@@ -8,9 +8,80 @@ namespace VirtualPet
 {
     class VirtualPet
     {
-        private string name = "unnamed dragon ";
-        static private string pet_type = "Dragon";
-        private string color = "no color ";
-        private int age = 0;
+       public enum LifeCycle {Baby, Tween, Teen, Adult};
+
+        //fields
+        private string petname;
+        private string pet_type;
+
+        //properties
+        public string Energy { get; set; }
+        public string SkillLevel { get; set; }
+        public string Food { get; set; }
+        public string Hunger { get; set; }
+        public string Tired { get; set; }
+
+        public string GetName
+            {
+            get {return this.petname; }
+            set { this.petname = value; }
+            }
+        public string GetPetType
+        {
+            get { return this.pet_type; }
+            set { this.pet_type = value; }
+        }
+
+        //public LifeCycle MyLifeCycle { get; set; }
+        //public string MyName { get; private set; }
+
+        //this.MyLifeCycle = LifeCycle.Baby;
+        //this.MyName = name;
+
+        //constructors
+        public VirtualPet()
+        {
+
+        }
+        public VirtualPet (string pet_type)
+        {
+            this.pet_type = pet_type;
+        }
+
+        //methods
+       
+        public void MenuDisplayNameandDragon ()
+        {
+            Console.WriteLine("Interaction Menu: " + petname + " The " + pet_type);
+        }
+        public void Play ()
+        {
+
+        }
+        public void Train ()
+        {
+
+        }
+        public void Hunt ()
+        {
+
+        }
+        public void Eat ()
+        {
+
+        }
+        public void Fly()
+        {
+
+        }
+        public void Sleep ()
+        {
+
+        }
+        public void Bathroom()
+        {
+
+        }
     }
 }
+
